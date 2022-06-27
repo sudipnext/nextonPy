f06322fd6745e257bee6eb03f90c1b56e6cadd18
sudipnext <parajulisudip123@outlook.com>
2022-06-27 18:27:10 +0545

    reading images and videos

--- opencv/read.py
+++ opencv/read.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')
+img = cv.imread('Photos/cat_large.jpg') #saving image to a variable called img
 
-cv.imshow('Cat', 'img')
+cv.imshow('Cat', img) #Displaying image
 
-cv.watikey(9)
+cv.waitKey(0) #Time is in miliseconds
---
+++ opencv/reading_videos.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+# Takes either integer if i am going to use webcam then if i am using videos i simply put the path of the video file
+capture = cv.VideoCapture(0)
+#I am reading videos as images by frame by frame while it's true it will continue to extract the files into two types one is a boolean and another is a frame
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+#Until i press the key d then the loop goes on an on and when i finally press d it breaks the loops and exits
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+#it's used to capture the release and destroy all windows for that i don't need that windows so
+capture.realease()
+cv.destroyAllWindows()
