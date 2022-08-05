4d69cda1df5ddf7e571e584da25d1aaa86f42c44
sudipnext <parajulisudip123@outlook.com>
2022-08-05 17:56:43 +0545

    Root Locus Finalized

--- ControlSystem/rootlocus.py
+++ ControlSystem/rootlocus.py
@@ -2,11 +2,17 @@ import control as ctl
 import numpy as np
 import matplotlib.pyplot as plt
 
-for kp in range(0,100):
-    den=[1, 8, 36, 80, kp]
+plt.grid()
+ran = np.linspace(0, 10, 1000)
+for k in np.arange(0.1, 50, 0.1):
+    den=[1, 4, 11, (14+k), (10+k)]
     s = np.roots(den)
     plt.plot(np.real(s), np.imag(s),'b*')
-    print(kp)
-    G = ctl.tf([kp], den)
+    G = ctl.tf([k], den)
     print(G)
+    #For Step Response 
+#uncomment the below line
+    # ran,dom = ctl.step_response(G,ran)
+    # plt.plot(ran,dom)
+
     plt.pause(0.1)
