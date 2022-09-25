2ecf6d71073ff4f1675dd7c9acecb91fdba180c7
sudipnext <parajulisudip123@outlook.com>
2022-09-25 19:42:20 +0545

    some updates

--- Linear_Regression/ph.py
+++
@@ -1,6 +0,0 @@
-import phonenumbers
-from phonenumbers import geocoder
-
-a=input("Enter a phone num")
-phonenumber = phonenumbers.parse(a)
-print(geocoder.description_for_number(phonenumber, 'en'))
