a547884de5e0169f5e5e7cb5626a7a7ec55f8439
sudipnext <parajulisudip123@outlook.com>
2022-10-03 20:45:33 +0545

    coin_filp simulation added and others arranged

--- ControlSystem/rootlocus.py
+++ ControlSystem/rootlocus.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 plt.grid()
 ran = np.linspace(0, 10, 1000)
 for k in np.arange(1, 300, 1):
-    den=[1, (6+k), (18-4*k)] 
+    den=[1, (6), (k)] 
     s = np.roots(den)
-    plt.plot(np.real(s), np.imag(s),'b*')
+    # plt.plot(np.real(s), np.imag(s),'b*')
     plt.title("Root Locus Plot")
     plt.xlabel("Sigma")
     plt.ylabel("jw")
     G = ctl.tf([k], den)
-    print(G)
+    # print(G)
     #For Step Response 
 #uncomment the below line
-    # ran,dom = ctl.step_response(G,ran)
-    # plt.plot(ran,dom)
+    ran,dom = ctl.step_response(G,ran)
+    plt.plot(ran,dom)
 
     plt.pause(0.1)
